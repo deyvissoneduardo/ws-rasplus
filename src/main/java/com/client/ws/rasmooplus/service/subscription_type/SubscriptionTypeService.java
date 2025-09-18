fe87db45e335aa356d22cc24d78e3640accfe959
deyvissoneduardo <deyvissoneduardo22@gmail.com>
2025-09-18 10:09:19 -0300

    api rest spring

---
+++ src/main/java/com/client/ws/rasmooplus/service/subscription_type/SubscriptionTypeService.java
@@ -0,0 +1,17 @@
+package com.client.ws.rasmooplus.service;
+
+import com.client.ws.rasmooplus.model.SubscriptionsType;
+
+import java.util.List;
+
+public interface SubscriptionTypeService {
+    List<SubscriptionsType> findAll();
+
+    SubscriptionsType findById(Long id);
+
+    SubscriptionsType create(SubscriptionsType subscriptionsType);
+
+    SubscriptionsType update(Long id, SubscriptionsType subscriptionsType);
+
+    void delete(Long id);
+}
---
+++ src/main/java/com/client/ws/rasmooplus/service/subscription_type/SubscriptionTypeServiceImpl.java
@@ -0,0 +1,4 @@
+package com.client.ws.rasmooplus.service.subscription_type;
+
+public class SubscriptionTypeImpl {
+}
