2b407d638d5766b30d9ba98c72b948ac9de4b286
deyvissoneduardo <deyvissoneduardo22@gmail.com>
2025-09-22 21:59:23 -0300

    integration process payment

--- src/test/java/com/client/ws/rasmooplus/integration/raspay/WsRaspayIntegrationImplTest.java
+++ src/test/java/com/client/ws/rasmooplus/integration/raspay/WsRaspayIntegrationImplTest.java
@@ -1,9 +1,14 @@
 package com.client.ws.rasmooplus.integration.raspay;
 
+import com.client.ws.rasmooplus.dto.wsraspay.CreditCardDto;
 import com.client.ws.rasmooplus.dto.wsraspay.CustomerDto;
+import com.client.ws.rasmooplus.dto.wsraspay.OrderDto;
+import com.client.ws.rasmooplus.dto.wsraspay.PaymentDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
 
 @SpringBootTest
 class WsRaspayIntegrationImplTest {
@@ -19,9 +24,14 @@ class WsRaspayIntegrationImplTest {
 
     @Test
     void createOrder() {
+        OrderDto dto = new OrderDto(null, BigDecimal.ZERO, "MONTH22", "68d16435e0c14a364821df1a");
+        wsRaspayIntegration.createOrder(dto);
     }
 
     @Test
     void processPayment() {
+        CreditCardDto creditCardDto = new CreditCardDto(123L, "05283390101", 06L, "1234123412341234", 0L, 2025L);
+        PaymentDto dto = new PaymentDto(creditCardDto, "68d16435e0c14a364821df1a", "68d1ee766a6ad674dbfaa791");
+        wsRaspayIntegration.processPayment(dto);
     }
 }
